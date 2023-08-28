af88bf27a14e934820696ee4192525d6097c5f75
marijokerud <mari.jokerud@gmail.com>
2023-08-28 15:01:49 +0200

    remove unused variables

--- code/01_read_data_permafrost.R
+++ code/01_read_data_permafrost.R
@@ -10,12 +10,13 @@ Sys.setlocale("LC_ALL", "Norwegian") #works with æøå or use "no_NB.utf8"
 info_dataset <- read_excel(path = "data/pals_database/20.2.2023_Uttrekk_info.xlsx", sheet = "Pals-lok-info", col_names = TRUE)
 
 #IMPORT DATA
-permafrostdepth <- read_excel(path = "data/pals_database/20.2.2023_Uttrekk_strukturlinjer.xlsx", sheet = "Teledyp", col_names = TRUE)
+permafrostdepth_raw <- read_excel(path = "data/pals_database/20.2.2023_Uttrekk_strukturlinjer.xlsx", sheet = "Teledyp", col_names = TRUE)
 ant_sprekker <- read_excel(path = "data/pals_database/20.2.2023_Uttrekk_strukturlinjer.xlsx", sheet = "antall-sprekker", col_names = TRUE)
 bredde_sprekker <- read_excel(path = "data/pals_database/20.2.2023_Uttrekk_strukturlinjer.xlsx", sheet = "bredde-sprekker", col_names = TRUE)
 
 #CLEAN DATA SET
-permafrostdepth <- permafrostdepth %>% 
+permafrostdepth <- permafrostdepth_raw %>% 
   rename(area= "Omr-info", site=Navn, year=År, line="Lj-nr", segment="Linje-del", depth ="Teledyp", height = "Høyde") %>% 
-  unite("lineID", line:segment, sep = ".", remove = FALSE)
+  unite("lineID", line:segment, sep = ".", remove = FALSE) %>% 
+  select(site, year, lineID, line, segment, depth, height)
 
