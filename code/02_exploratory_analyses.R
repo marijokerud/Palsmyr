b18ce057b3334e0bc7bfd43af5c116d995d5c529
marijokerud <mari.jokerud@gmail.com>
2023-08-29 11:49:17 +0200

    added for species lines

--- code/02_exploratory_analyses.R
+++ code/02_exploratory_analyses.R
@@ -12,3 +12,12 @@ data_summary_segment <- structurelinePSL %>%
   group_by(site, line) %>% 
   summarise(tot = sum(no))
   
+  data_summary_speciesline <- speciesline %>%
+    select(site, line) %>% 
+    unique() %>% 
+    mutate(no = 1) %>%
+    group_by(site) %>% 
+    summarise(tot = sum(no))
+  group_by(site, line) %>% 
+    summarise(tot = sum(no))
+  
