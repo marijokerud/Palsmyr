6b75176ef8d0f952d0a0f00bf932bd6c849f01a5
marijokerud <mari.jokerud@gmail.com>
2023-08-30 15:06:15 +0200

    adding different models to check them out. m1 is the correct one base on the data

--- code/02_regression_analyses.R
+++ code/02_regression_analyses.R
@@ -3,26 +3,31 @@ library(Rcurl)
 library(lme4)
 library(ggplot2)
 
-structurelinePSL$pals = factor(structurelinePSL$pals)
-
-summary(structurelinePSL$pals) #check ratio
+#structurelinePSL$pals = factor(structurelinePSL$pals)
+#summary(structurelinePSL$pals) #check ratio
 
 m1 <- glmer(pals ~ year * site + (1 | line), 
           family = binomial(link = "logit"),
           data = structurelinePSL)
 summary(m1)
 
-m2 <- glmer(pals ~ year * site + (1 | line) + (1 | overdisp), 
+m2 <- glmer(pals ~ year + site + (1 | line), 
             family = binomial(link = "logit"),
             data = structurelinePSL)
 summary(m2)
 
-AIC(logLik(m1))
-AIC(logLik(m2)) # AIC is higher than m1, m2 generated a warning because the model is overfitted (overdisp variance is 0) – that is, the random effects structure is too complex to be supported by the data
+m3 <- glmer(pals ~ year + (1 | line), 
+            family = binomial(link = "logit"),
+            data = structurelinePSL)
+summary(m3)
 
-cleanup = theme(panel.grid.major = element_blank(), 
-                panel.grid.minor = element_blank(), 
-                panel.background = element_blank(), 
-                axis.line = element_line(colour = "black"),
-                legend.key = element_rect(fill = "white"),
-                text = element_text(size = 15))
+m4 <- glmer(pals ~ year * site + (1 | line) + (1 | overdisp), 
+            family = binomial(link = "logit"),
+            data = structurelinePSL)
+summary(m4)
+
+AIC(logLik(m1))
+AIC(logLik(m2))
+AIC(logLik(m3))
+AIC(logLik(m4)) # AIC is higher than m1, m4 generated a warning because the model is overfitted (overdisp variance is 0) – that is, the random effects structure is too complex to be supported by the data
+
